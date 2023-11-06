4349fb7ea3b348b3027903e4eea79f64263764b4
Nadim Metwalli <nadimmetwalli10@gmail.com>
2023-11-06 13:04:59 +0200

    commit

---
+++ Kood_metwalli/JS_Harjutused/main2.js
@@ -0,0 +1,18 @@
+function harjutus1(nimi) {
+    console.log(`Tere, ${Nadim Metwalli}!`);
+}
+
+// Näiteks:
+harjutus1("Nadim");
+JS_harjutus 
+
+let myName = 'Nadim';
+document.write('TERE, ' + myName Nadim Metwalli.toUpperCase() + '!');
+
+// Mõned stringi meetodid:
+console.log(`Kenno  `.slice(0, 3))
+console.log("Õun, Banaan, Kiivi".substr(5))
+
+if (myName.length > 3) {
+    console.log('jep');
+}
